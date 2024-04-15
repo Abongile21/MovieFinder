91a9e2f246fb0bf52258bc03298768f00dc16ce8
Abongile21 <abongiletheo00@gmail.com>
2024-04-15 13:02:03 +0200

    modified the api url on the js file

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ async function searchMovie() {
         const response = await fetch(`https://dummyapi.online/api/movies?name=${movieName}`);
         const data = await response.json();
 
-        if (data.movie==movieName && data.length > 0) {
-            const movieHTML = data.map(movie => {
+        const matchingMovies = data.filter(movie => movie.movie.toLowerCase() === movieName.toLowerCase());
+
+        if (matchingMovies.length > 0) {
+            const movieHTML = matchingMovies.map(movie => {
                 return `<div class="movieItem">
                             <h3>${movie.movie}</h3>
-                            <img src="${movie.image}" alt="${movie.name}">
-                            <p>Rating:${movie.rating}</p>
-                            <button id="watchBtn"><a href="${movie.imd_url}">Watch</a></button>
+                            <img src="${movie.image}" alt="${movie.movie}">
+                            <p>Rating: ${movie.rating}</p>
+                            <button id="watchBtn"><a href="${movie.imdb_url}" target="_blank">Watch</a></button>
                         </div>`;
             }).join('');
 
